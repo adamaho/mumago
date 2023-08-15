e7f29302ff99a0cb739249c6151505f0f77b99d1
adamaho <aho.adam01@gmail.com>
2023-08-15 17:54:41 -0400

    (chore): convert folder structure to be more idiomatic go (#7)

--- cmd/todos/main.go
+++ cmd/todos/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/google/uuid"
+
+	"mumago/internal/realtime"
 )
 
 func main() {
@@ -16,14 +16,14 @@ func main() {
 
 	r.Use(middleware.Logger)
 
-	rt := Realtime{Clients: make([]*Client, 0)}
+	rt := realtime.Realtime{Clients: make([]*realtime.Client, 0)}
 
 	r.Group(func(r chi.Router) {
 		r.Get("/subscribe", func(w http.ResponseWriter, r *http.Request) { subscribe(w, r, &rt) })
 		r.Post("/publish/{message}", func(w http.ResponseWriter, r *http.Request) { publish(w, r, &rt) })
 	})
 
-	log.Println("starting server at https://localhost:3000")
+	log.Println("starting todos server at https://localhost:3000")
 
 	err := http.ListenAndServeTLS("localhost:3000", "cert.pem", "key.pem", r)
 
@@ -32,7 +32,7 @@ func main() {
 	}
 }
 
-func subscribe(w http.ResponseWriter, r *http.Request, rt *Realtime) {
+func subscribe(w http.ResponseWriter, r *http.Request, rt *realtime.Realtime) {
 	// init the context
 	ctx := r.Context()
 
@@ -70,63 +70,10 @@ func subscribe(w http.ResponseWriter, r *http.Request, rt *Realtime) {
 	}
 }
 
-func publish(w http.ResponseWriter, r *http.Request, rt *Realtime) {
+func publish(w http.ResponseWriter, r *http.Request, rt *realtime.Realtime) {
 	msg := chi.URLParam(r, "message")
 	for _, client := range rt.Clients {
-		*client.channel <- msg
+		*client.Channel <- msg
 	}
 	fmt.Fprintf(w, "Message sent: %s", msg)
-}
-
-type Payload struct {
-	Data []byte
-}
-
-// Creates a new plain text payload
-func NewPayload(s string) *Payload {
-	return &Payload{Data: []byte(s)}
-}
-
-// Creates a new json payload
-func NewJsonPayload(j struct{}) (*Payload, error) {
-	d, err := json.Marshal(j)
-
-	if err != nil {
-		log.Println("failed to marshal struct:", err)
-		return nil, err
-	}
-
-	return &Payload{Data: d}, nil
-}
-
-// Represents a single client connection
-type Client struct {
-	id      uuid.UUID
-	channel *chan string
-}
-
-// Holds all of the active client connections
-type Realtime struct {
-	Clients []*Client
-}
-
-// Adds a new client to the connection list
-func (r *Realtime) AddClient(ch *chan string) uuid.UUID {
-	id := uuid.New()
-	client := Client{id: id, channel: ch}
-	r.Clients = append(r.Clients, &client)
-	return id
-}
-
-// Removes a client from the connection list
-func (r *Realtime) RemoveClient(clientID uuid.UUID) {
-	newClients := make([]*Client, 0)
-
-	for _, c := range r.Clients {
-		if c.id != clientID {
-			newClients = append(newClients, c)
-		}
-	}
-
-	r.Clients = newClients
 }
