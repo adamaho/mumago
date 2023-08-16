362205fcbf284869282050e35a887e0988737cef
adamaho <aho.adam01@gmail.com>
2023-08-15 21:54:31 -0400

    (feat): generalizing some of the streaming logic

--- internal/realtime/realtime.go
+++ internal/realtime/realtime.go
@@ -1,6 +1,12 @@
 package realtime
 
-import "github.com/google/uuid"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 // Represents a single client connection
 type Client struct {
@@ -13,28 +19,74 @@ type Realtime struct {
 	Clients []*Client
 }
 
-// TODO: Create New function for creating a realtime struct
-// TODO: Create Streaming function for the streaming endpoint
-// TODO: It should handle non-streaming support as well and streaming should be supported based on a header
-// TODO: Should have a mutation function maybe?
+// Creates a new instance of realtime
+func New() Realtime {
+	return Realtime{Clients: make([]*Client, 0)}
+}
+
+// Handles creating a stream and channel if the X-Muma-Stream header is set. If header
+// is not set, the raw json message is returned to the user like a standard REST api.
+func (rt *Realtime) Stream(w http.ResponseWriter, r *http.Request, d json.RawMessage) {
+	ctx := r.Context()
+
+	s := r.Header.Get("X-Muma-Stream")
+	if s == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(d)
+		return
+	}
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		// TODO: come up with a generic error struct to return instead of a plain string
+		http.Error(w, "Streaming not supported!", http.StatusInternalServerError)
+		return
+	}
+
+	ch := make(chan string, 10)
+	defer close(ch)
+
+	clientID := rt.AddClient(&ch)
+
+	w.Header().Set("Transfer-Encoding", "chunked")
+	w.Header().Set("Content-Type", "application/json+ndjsonpatch")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", d)
+	flusher.Flush()
+
+	for {
+		select {
+		case <-ctx.Done():
+			rt.RemoveClient(clientID)
+			return
+		case value := <-ch:
+			if value != "" {
+				// TODO: Make this support a jsonpatch diff value from the channel
+				fmt.Fprintf(w, "data: %s\n", value)
+			}
+			flusher.Flush()
+		}
+	}
+}
 
 // Adds a new client to the connection list
-func (r *Realtime) AddClient(ch *chan string) uuid.UUID {
+func (rt *Realtime) AddClient(ch *chan string) uuid.UUID {
 	id := uuid.New()
 	client := Client{id: id, Channel: ch}
-	r.Clients = append(r.Clients, &client)
+	rt.Clients = append(rt.Clients, &client)
 	return id
 }
 
 // Removes a client from the connection list
-func (r *Realtime) RemoveClient(clientID uuid.UUID) {
+func (rt *Realtime) RemoveClient(clientID uuid.UUID) {
 	newClients := make([]*Client, 0)
 
-	for _, c := range r.Clients {
+	for _, c := range rt.Clients {
 		if c.id != clientID {
 			newClients = append(newClients, c)
 		}
 	}
 
-	r.Clients = newClients
+	rt.Clients = newClients
 }
