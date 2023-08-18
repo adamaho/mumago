c0ce2fc62d024a711433d653d631a73f303e3ef4
adamaho <aho.adam01@gmail.com>
2023-08-17 21:40:37 -0400

    removing unneeded print statement

--- cmd/todos/main.go
+++ cmd/todos/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -75,7 +74,6 @@ func createTodo(w http.ResponseWriter, r *http.Request, rt *realtime.Realtime, t
 	patchJson, err := json.Marshal(patch)
 
 	if err != nil {
-		fmt.Println("err", err)
 		http.Error(w, "Failed to marshal json", http.StatusInternalServerError)
 		return
 	}
