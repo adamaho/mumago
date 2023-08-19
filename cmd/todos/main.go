5a0acb11135ec7030043f086fb8624fc6a62af3b
adamaho <aho.adam01@gmail.com>
2023-08-19 14:48:15 -0400

    (feat): create internal/api package for creating endpoints

--- cmd/todos/main.go
+++ cmd/todos/main.go
@@ -1,46 +1,31 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/google/uuid"
 
+	"mumago/internal/api"
 	"mumago/internal/db"
 	"mumago/internal/realtime"
 )
 
-type Todo struct {
-	TodoId uuid.UUID `json:"todo_id"`
-	Task   string    `json:"task"`
-}
-
-type Todos struct {
-	Data []Todo `json:"data"`
-}
-
-// Adds a todo to the database
-func (t *Todos) AddTodo(todo Todo) Todos {
-	t.Data = append(t.Data, todo)
-	return *t
-}
-
 func main() {
 	r := chi.NewRouter()
 
-	todos := Todos{Data: make([]Todo, 0)}
-
-	db.New().Connect()
+	db := db.New().Connect()
+	todos := api.NewTodos()
 
 	r.Use(middleware.Logger)
 
 	r.Group(func(r chi.Router) {
 		rt := realtime.New()
-		r.Get("/todos", func(w http.ResponseWriter, r *http.Request) { getTodos(w, r, &rt, &todos) })
-		r.Post("/todos/{task}", func(w http.ResponseWriter, r *http.Request) { createTodo(w, r, &rt, &todos) })
+		todosApi := api.NewTodosApi(db, &rt, &todos)
+
+		r.Get("/todos", todosApi.GetTodos)
+		r.Post("/todos/{task}", todosApi.CreateTodo)
 	})
 
 	log.Println("starting todos server at https://localhost:3000")
@@ -50,24 +35,4 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
-}
-
-func getTodos(w http.ResponseWriter, r *http.Request, rt *realtime.Realtime, todos *Todos) {
-	// TODO: get todos from db instead
-	d, err := json.Marshal(todos)
-	if err != nil {
-		http.Error(w, "Failed to parse json", http.StatusInternalServerError)
-		return
-	}
-	rt.Stream(w, r, d)
-}
-
-func createTodo(w http.ResponseWriter, r *http.Request, rt *realtime.Realtime, todos *Todos) {
-	task := chi.URLParam(r, "task")
-	ts := todos.AddTodo(Todo{TodoId: uuid.New(), Task: task})
-	target, _ := json.Marshal(ts)
-
-	// TODO: handle error
-
-	rt.PublishPatch(w, target)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module mumago
 go 1.21.0
 
 require (
+	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.5.1
