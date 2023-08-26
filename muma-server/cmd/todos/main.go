397708115a7d7c94273c72d56f01587bc7d4f772
adamaho <aho.adam01@gmail.com>
2023-08-26 12:00:21 -0700

    (feat): connects client to server and supports json patch streams via http2

--- muma-server/cmd/todos/main.go
+++ muma-server/cmd/todos/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 
 	"muma/internal/api"
 	"muma/internal/db"
@@ -17,6 +18,14 @@ func main() {
 	db := db.New().Connect()
 
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Muma-Stream"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Group(func(r chi.Router) {
 		todosApi := api.NewTodosApi(db)
